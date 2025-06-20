3d629e26f2293dabfff911fa0b8a1f316e8a82d1
Varun Rajput <varun.rajput@h2o.ai>
2025-06-20 16:34:51 +0530

    tf state backend

--- infra/terraform-deployments/providers.tf
+++ infra/terraform-deployments/providers.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-varun-blogs"
+    key            = "tf-state.tfstate"
+    profile        = "varun_blogs"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
