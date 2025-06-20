f87fac3376db7989a185d0e63e49e1321d8dd11a
Varun Rajput <varun.rajput@h2o.ai>
2025-06-20 16:47:06 +0530

    bucket location correction

--- infra/terraform-deployments/providers.tf
+++ infra/terraform-deployments/providers.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-varun-blogs"
     key            = "tf-state.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"
     encrypt        = true
   }
 }
